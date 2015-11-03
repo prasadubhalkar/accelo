d4e75b0f3f7d64f98e7b2dc29de73de027045b0d
Prasad <prasadubhalkar@gmail.com>
2015-11-02 22:22:38 -0600

    adding network check

--- www/js/controllers/trackController.js
+++ www/js/controllers/trackController.js
@@ -3,30 +3,48 @@
  * $scope               [Object]  Controller scope
  * $cordovaGeolocation  [Factory] Apache Cordova Factory Wrapper around navigator geolocation
  */
-accelo.controller("trackController",function($scope,$cordovaGeolocation){
-    $scope.count    = 0;
-    $scope.speed    = 0;
-    $scope.distance = 0;
-    $scope.inGreen  = false;
-    $scope.inYellow = false;
-    $scope.inRed    = false;
-    $scope.pos      = [];    
+accelo.controller("trackController",function($scope,$cordovaGeolocation,$rootScope, $cordovaNetwork){
+    $scope.count    = 0;        
+    $scope.speed    = 0;        
+    $scope.currDist = 0;        
+    $scope.distance = 0;        
+    $scope.inGreen  = false;    
+    $scope.inYellow = false;    
+    $scope.inRed    = false;    
     $scope.speeds   = [];
     $scope.watch    = {};
+    $scope.path     = {};
     google.maps.event.addDomListener(window,'load',$scope.init);
 
+    /**
+     * [init function will initiate the map]
+     * @return {[type]} [description]
+     */
     $scope.init = function(){
         var latLng = new google.maps.LatLng(0,0);
+
         var mapOptions = {
             center : latLng,
             zoom : 16,
             mapTypeId : google.maps.MapTypeId.ROADMAP
         };
         $scope.map          = new google.maps.Map(document.getElementById("map"),mapOptions);
-        $scope.infoWindow   = new google.maps.InfoWindow({map:$scope.map});
+
+        $scope.path = new google.maps.Polyline({
+            path : [],
+            geodesic : true,
+            strokeColor : '#0000FF',
+            strokeOpacity : 1, 
+            strokeWeight : 4
+        });
+
         $scope.watchLocation();
     }
 
+    /**
+     * [watchLocation When the watch location is been clicked]
+     * @return {[type]} [description]
+     */
     $scope.watchLocation = function(){
         $scope.watch = $cordovaGeolocation.watchPosition(settings.geoOptions);
         $scope.watch.then(
@@ -38,18 +56,23 @@ accelo.controller("trackController",function($scope,$cordovaGeolocation){
         );
     }
 
+    /**
+     * [updateLocation is a callback for the watchlocation event]
+     * @param  {[type]} result [latitude and longitude from navigator geolocation]
+     * @return {[type]}        [description]
+     */
     $scope.updateLocation = function(result){
-        $scope.count    += 1;
+        var arr = $scope.path.getPath(),
+            pos = { 
+                lat : result.coords.latitude, 
+                lng : result.coords.longitude 
+            };
 
         positions.setCords(result.coords.latitude,result.coords.longitude);
         
-        var pos = {
-            lat : result.coords.latitude,
-            lng : result.coords.longitude
-        };
+        arr.push(new google.maps.LatLng(pos.lat, pos.lng));
 
-        $scope.pos.push(pos);
-
+        $scope.count    += 1;
         $scope.map.setCenter(pos);
 
         if($scope.count == 1) {
@@ -71,14 +94,7 @@ accelo.controller("trackController",function($scope,$cordovaGeolocation){
             });    
 
         } else {
-            var path = new google.maps.Polyline({
-                path : $scope.pos,
-                geodesic : true,
-                strokeColor : '#0000FF',
-                strokeOpacity : 1, 
-                strokeWeight : 4
-            });
-
+            path.setPath(arr);
             path.setMap($scope.map);
         }
         
@@ -87,24 +103,37 @@ accelo.controller("trackController",function($scope,$cordovaGeolocation){
 
         $scope.speeds.push($scope.speed);
 
-        $scope.distance += $scope.result.distance;
-        $scope.distance = Math.round($scope.distance);
+        $scope.currDist += $scope.result.distance;
+        $scope.distance = $scope.currDist.toFixed(2);
 
-        if($scope.speed < 40){
+        $scope.updateSpeedDisplay($scope.speed)
+    }
+
+    /**
+     * [updateSpeedDisplay will update the speed color]
+     * @param  {[type]} speed [description]
+     * @return {[type]}       [description]
+     */
+    $scope.updateSpeedDisplay = function(speed){
+        if(speed < 40){
             $scope.inGreen = true;
             $scope.inYellow = false;
             $scope.inRed = false;
-        } else if($scope.speed > 40 && $scope.speed < 70) {
+        } else if(speed > 50 && speed < 70) {
             $scope.inGreen = false;
             $scope.inYellow = true;
             $scope.inRed = false;
-        } else if($scope.speed > 70){
+        } else if(speed > 70){
             $scope.inGreen = false;
             $scope.inYellow = false;
             $scope.inRed = true;
         }
     }
 
+    /**
+     * [stopWatching will clear the watch for the current geolocation watch]
+     * @return {[type]} [description]
+     */
     $scope.stopWatching = function(){
         try {
             $scope.watch.clearWatch();
@@ -125,5 +154,14 @@ accelo.controller("trackController",function($scope,$cordovaGeolocation){
         $scope.distance = 0;
     }
 
-    $scope.init();
+    document.addEventListener("deviceready", function () {
+        var isOnline = $cordovaNetwork.isOnline();
+
+        if(isOnline) {
+            $scope.init();    
+        } else {
+            alert("Device is offline");
+        }
+        
+    },false);
 });
