6753e975750e74a726d7f43cebdbf1509962677c
Prasad <prasadubhalkar@gmail.com>
2015-11-09 21:27:40 -0600

    initial settings

--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -2,14 +2,17 @@ function homeController($scope,$cordovaGeolocation,$cordovaNetwork,$state){
 	var self = this;
 		self.showTrack = true;
 		self.showSettings = false;
+		self.header = "Home";
 
 	self.swipeLeft = function(){
 		self.showTrack = false;
 		self.showSettings = true;		
+		self.header = "Settings";
 	}
 
 	self.swipeRight = function(){
 		self.showTrack = true;
 		self.showSettings = false;
+		self.header = "Home";
 	}
 }
--- www/js/controllers/settingsController.js
+++ www/js/controllers/settingsController.js
@@ -1,3 +1,24 @@
 function settingsController($scope,$cordovaGeolocation,$cordovaNetwork,$state){
-	var self = this;
+	var self 			= this;
+		self.kmMode 	= false;
+		self.saveMode	= true;
+		self.walkMode	= false;
+		self.avgMode	= true;
+
+	self.changeKM = function() {
+		console.log(self.kmMode);
+	}
+
+	self.changeSaveMode = function() {
+		console.log(self.saveMode);
+	}
+
+	self.changeWalkMode = function(){
+		console.log(self.walkMode);
+	}
+
+	self.changeAvgMode = function() {
+		console.log(self.avgMode);	
+	}
+
 }
